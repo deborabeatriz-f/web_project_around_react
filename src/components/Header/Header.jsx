4b69eca12aad0943356637714f778a776a01a0ab
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-06 13:02:22 -0300

    move section from header jsx to main jsx file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import logo from "../../images/logo_around.png";
-import avatar from "../../images/jacques_cousteau.png";
+
 
 export default function Header() {
   return (
@@ -8,19 +8,7 @@ export default function Header() {
         <img src={logo} alt="Logo Around The U.S." className="nav__image" />
       </nav>
 
-      <section className="profile">
-        <div className="profile__card-image">
-          <img src={avatar} alt="Jacques Cousteau" className="profile__image" />
-          <button className="profile__btn-avatar"></button>
-        </div>
-        <div className="profile__card">
-          <h2 className="profile__title">Jacques Cousteau</h2>
-          <button className="profile__button-edit"></button>
-          <p className="profile__subtitle">Explorador</p>
-        </div>
-
-        <button className="profile__button-add"></button>
-      </section>
+     
     </header>
   );
 }
