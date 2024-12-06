8cf227e51603fdd48565dcb4ffe11a4314fcea01
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-06 13:05:03 -0300

    update Main jsx file (added funcitons, added section into main and added popup)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,52 @@
 import heart from "../../images/heart_icon.png";
 import heartClicked from "../../images/heart-clicked.png";
+import avatar from "../../images/jacques_cousteau.png";
+import { useState } from "react";
+import Popup from "./components/Popup/Popup";
+import NewCard from "./components/Popup/components/NewCard/NewCard";
+import EditProfile from "./components/Popup/components/EditProfile/EditProfile";
+import EditAvatar from "./components/Popup/components/EditAvatar/EditAvatar";
 
 export default function Main() {
+  const [popup, setPopup] = useState(null);
+
+  const newCardPopup = { title: "New card", children: <NewCard /> };
+  const editProfilePopup = { title: "Edit Profile", children: <EditProfile /> };
+  const editAvatarPopup = { title: "Edit Avatar", children: <EditAvatar /> };
+
+  function handleOpenPopup(popup) {
+    setPopup(popup);
+  }
+
+  function handleClosePopup() {
+    setPopup(null);
+  }
+
   return (
     <main className="grid__content page__container">
+      <section className="profile">
+        <div className="profile__card-image">
+          <img src={avatar} alt="Jacques Cousteau" className="profile__image" />
+          <button
+            className="profile__btn-avatar"
+            onClick={() => handleOpenPopup(editAvatarPopup)}
+          ></button>
+        </div>
+        <div className="profile__card">
+          <h2 className="profile__title">Jacques Cousteau</h2>
+          <button
+            className="profile__button-edit"
+            onClick={() => handleOpenPopup(editProfilePopup)}
+          ></button>
+          <p className="profile__subtitle">Explorador</p>
+        </div>
+
+        <button
+          className="profile__button-add"
+          onClick={() => handleOpenPopup(newCardPopup)}
+        ></button>
+      </section>
+
       <template className="grid__template">
         <div className="grid__card">
           <button className="grid__card-delete"></button>
@@ -23,6 +66,11 @@ export default function Main() {
           </div>
         </div>
       </template>
+      {popup && (
+        <Popup onClose={handleClosePopup} title={popup.title}>
+          {popup.children}
+        </Popup>
+      )}
     </main>
   );
 }
