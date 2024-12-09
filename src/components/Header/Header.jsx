7974993f9685d5ba4cb1a7a28bb2786cf289d603
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-09 10:18:34 -0300

    rename class from nav to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,9 @@
 import logo from "../../images/logo_around.png";
 
-
 export default function Header() {
   return (
     <header className="header page__container">
-      <nav className="nav">
-        <img src={logo} alt="Logo Around The U.S." className="nav__image" />
-      </nav>
-
-     
+      <img src={logo} alt="Logo Around The U.S." className="header__image" />
     </header>
   );
 }
