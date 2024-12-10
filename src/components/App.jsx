f4abe5cf20d7183b55996df968c495af89689f53
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-10 13:40:30 -0300

    remove popup delete card from app jsx file - this feature is not to be implemented in this sprint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,22 +9,6 @@ function App() {
         <Header />
         <Main />
         <Footer />
-
-        {/* <!-- POPUP DELETE CARD --> */}
-        {/* <div className="popup__container popup__container-deleteCard">
-        <div className="popup__card">
-          <h3 className="popup__title">Tem certeza?</h3>
-          <form className="popup__input input-deleteCard" noValidate>
-            <button type="submit" className="input__submit input__submit-delete">
-              Sim
-            </button>
-          </form>
-          <button
-            className="popup__button-close button-closeDelete"
-            draggable="true"
-          ></button>
-        </div>
-      </div> */}
       </div>
     </>
   );
