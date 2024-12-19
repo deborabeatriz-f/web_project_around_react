447f2e98635049e7583ecbd57ef9c1f9a3a79c28
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-19 12:44:02 -0300

    update App file, api user info, useState and useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,23 @@
 import Header from "./Header/Header.jsx";
 import Main from "./Main/Main.jsx";
 import Footer from "./Footer/Footer.jsx";
+import { useState, useEffect } from "react";
+import api from "../utils/Api.js";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    api
+      .getUserInfo()
+      .then((data) => {
+        setCurrentUser(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div className="page">
       <Header />
@@ -13,3 +28,23 @@ function App() {
 }
 
 export default App;
+
+{
+  /* <!-- POPUP DELETE CARD --> */
+}
+{
+  /* <div className="popup__container popup__container-deleteCard">
+        <div className="popup__card">
+          <h3 className="popup__title">Tem certeza?</h3>
+          <form className="popup__input input-deleteCard" noValidate>
+            <button type="submit" className="input__submit input__submit-delete">
+              Sim
+            </button>
+          </form>
+          <button
+            className="popup__button-close button-closeDelete"
+            draggable="true"
+          ></button>
+        </div>
+      </div> */
+}
