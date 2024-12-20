06556a9f17303f33927f868b79450a0c903a7ed8
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-20 12:30:13 -0300

    update App jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,25 @@ import Footer from "./Footer/Footer.jsx";
 import { useState, useEffect } from "react";
 import api from "../utils/Api.js";
 import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
+import { CardsContext } from "../contexts/CardsContext.js";
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
+
+  const [popup, setPopup] = useState(null);
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    api
+      .getInitialCards()
+      .then((data) => {
+        setCards(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   useEffect(() => {
     api
@@ -19,13 +35,88 @@ function App() {
       });
   }, []);
 
+  const handleUpdateUser = (data) => {
+    (async () => {
+      await api.setUserInfo(data).then((newData) => {
+        setCurrentUser(newData);
+        handleClosePopup();
+      });
+    })();
+  };
+
+  const handleUpdateAvatar = (data) => {
+    (async () => {
+      await api.setAvatar(data).then((newData) => {
+        setCurrentUser(newData);
+        handleClosePopup();
+      });
+    })();
+  };
+
+  const handleAddPlaceSubmit = (data) => {
+    (async () => {
+      await api.newCard(data).then((newCard) => {
+        setCards([newCard, ...cards]);
+        handleClosePopup();
+      });
+    })();
+  };
+
+  async function handleCardLike(card) {
+    const isLiked = card.isLiked;
+    if (isLiked) {
+      await api.unlikedCard(card._id).then((newCard) => {
+        setCards((state) =>
+          state.map((currentCard) =>
+            currentCard._id === card._id ? newCard : currentCard
+          )
+        );
+      });
+    } else {
+      await api.likedCard(card._id).then((newCard) => {
+        setCards((state) =>
+          state.map((currentCard) =>
+            currentCard._id === card._id ? newCard : currentCard
+          )
+        );
+      });
+    }
+  }
+
+  async function handleCardDelete(card) {
+    await api.deleteCard(card._id).then(() => {
+      setCards((state) =>
+        state.filter((currentCard) => currentCard._id !== card._id)
+      );
+    });
+  }
+
+  function handleOpenPopup(popup) {
+    setPopup(popup);
+  }
+
+  function handleClosePopup() {
+    setPopup(null);
+  }
+
   return (
     <div className="page">
-      <CurrentUserContext.Provider value={currentUser}>
-        <Header />
-        <Main />
-        <Footer />
-      </CurrentUserContext.Provider>
+      <CardsContext.Provider value={handleAddPlaceSubmit}>
+        <CurrentUserContext.Provider
+          value={{ currentUser, handleUpdateUser, handleUpdateAvatar }}
+        >
+          <Header />
+          <Main
+            handleOpenPopup={handleOpenPopup}
+            handleClosePopup={handleClosePopup}
+            popup={popup}
+            handleCardLike={handleCardLike}
+            handleCardDelete={handleCardDelete}
+            cards={cards}
+          />
+          <Footer />
+        </CurrentUserContext.Provider>
+      </CardsContext.Provider>
     </div>
   );
 }
