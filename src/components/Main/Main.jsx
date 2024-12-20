ee278751684e0fe1ed0dcdfd3413774a354f3d42
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-20 08:55:29 -0300

    update main jsx file, add handle card like and handle card delete

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,17 @@
 import avatar from "../../images/jacques_cousteau.png";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Popup from "./components/Popup/Popup";
 import NewCard from "./components/Popup/components/NewCard/NewCard";
 import EditProfile from "./components/Popup/components/EditProfile/EditProfile";
 import EditAvatar from "./components/Popup/components/EditAvatar/EditAvatar";
 import Card from "./components/Card/Card";
 import api from "../../utils/Api.js";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
 
 export default function Main() {
   const [popup, setPopup] = useState(null);
+
+  const currentUser = useContext(CurrentUserContext);
 
   const newCardPopup = { title: "New card", children: <NewCard /> };
   const editProfilePopup = { title: "Edit Profile", children: <EditProfile /> };
@@ -35,23 +38,61 @@ export default function Main() {
     setPopup(null);
   }
 
+  async function handleCardLike(card) {
+    const isLiked = card.isLiked;
+    if (isLiked) {
+      await api.unlikedCard(card._id).then((newCard) => {
+        setCards((state) =>
+          state.map((currentCard) =>
+            currentCard._id === card._id ? newCard : currentCard
+          )
+        );
+      });
+    } else {
+      await api.likedCard(card._id).then((newCard) => {
+        setCards((state) =>
+          state.map((currentCard) =>
+            currentCard._id === card._id ? newCard : currentCard
+          )
+        );
+      });
+    }
+  }
+
+  async function handleCardDelete(card) {
+    await api.deleteCard(card._id).then(() => {
+       setCards((state) => 
+        state.filter((currentCard) => 
+          currentCard._id !== card._id
+        ))
+    });
+  }
+
   return (
     <main className="page__container">
       <section className="profile">
         <div className="profile__card-image">
-          <img src={avatar} alt="Jacques Cousteau" className="profile__image" />
+          <img
+            src={currentUser.avatar || avatar}
+            alt={currentUser.name || "Jacques Cousteau"}
+            className="profile__image"
+          />
           <button
             className="profile__btn-avatar"
             onClick={() => handleOpenPopup(editAvatarPopup)}
           ></button>
         </div>
         <div className="profile__card">
-          <h2 className="profile__title">Jacques Cousteau</h2>
+          <h2 className="profile__title">
+            {currentUser.name || "Jacques Cousteau"}
+          </h2>
           <button
             className="profile__button-edit"
             onClick={() => handleOpenPopup(editProfilePopup)}
           ></button>
-          <p className="profile__subtitle">Explorador</p>
+          <p className="profile__subtitle">
+            {currentUser.about || "Explorador"}
+          </p>
         </div>
 
         <button
@@ -62,7 +103,13 @@ export default function Main() {
 
       <section className="grid">
         {cards.map((card) => (
-          <Card key={card._id} card={card} handleOpenPopup={handleOpenPopup} />
+          <Card
+            key={card._id}
+            card={card}
+            handleOpenPopup={handleOpenPopup}
+            handleCardLike={handleCardLike}
+            handleCardDelete={handleCardDelete}
+          />
         ))}
       </section>
 
