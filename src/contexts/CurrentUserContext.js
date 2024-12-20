ee0c9b25862415226856ed809be6881908898d0a
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-20 12:29:33 -0300

    update current user context

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,10 +1,3 @@
 import {createContext} from "react";
-// import avatar from "../images/jacques_cousteau.png";
 
 export const CurrentUserContext = createContext();
-
-// export const CurrentUser =  {
-//    name: "Jacques Cousteau",
-//    about: "Explorado",
-//    avatar: {avatar}
-// };
