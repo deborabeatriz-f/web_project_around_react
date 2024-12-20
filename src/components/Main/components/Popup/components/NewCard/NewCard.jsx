0758cb25fb07b0c30cbe240b09d502158b9c57cc
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-20 12:32:04 -0300

    update new card, add useState and useContext

--- src/components/Main/components/Popup/components/NewCard/NewCard.jsx
+++ src/components/Main/components/Popup/components/NewCard/NewCard.jsx
@@ -1,6 +1,32 @@
+import { useState, useContext } from "react";
+import { CardsContext } from "../../../../../../contexts/CardsContext";
+
 export default function NewCard() {
+  const handleAddPlaceSubmit = useContext(CardsContext);
+
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleLink = (event) => {
+    setLink(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    handleAddPlaceSubmit({ name, link });
+  };
+
   return (
-    <form className="popup__input input-image" noValidate>
+    <form
+      className="popup__input input-image"
+      noValidate
+      onSubmit={handleSubmit}
+    >
       <div className="input__wrap">
         <input
           className="input__text input__text-title"
@@ -11,6 +37,8 @@ export default function NewCard() {
           minLength="2"
           maxLength=" 30"
           required
+          value={name}
+          onChange={handleName}
         />
         <p className="input__errorMessage"></p>
       </div>
@@ -22,14 +50,12 @@ export default function NewCard() {
           type="url"
           placeholder="Link de imagem"
           required
+          value={link}
+          onChange={handleLink}
         />
         <p className="input__errorMessage"></p>
       </div>
-      <button
-        type="submit"
-        className="input__submit input__submit-add"
-        disabled
-      >
+      <button type="submit" className="input__submit input__submit-add">
         Criar
       </button>
     </form>
