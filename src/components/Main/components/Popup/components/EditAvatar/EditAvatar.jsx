fb746da6bd6801a5014a7e7f8925a7f8f8bc1c3f
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-12-20 12:30:47 -0300

    update edit avatar, add useContext and useState

--- src/components/Main/components/Popup/components/EditAvatar/EditAvatar.jsx
+++ src/components/Main/components/Popup/components/EditAvatar/EditAvatar.jsx
@@ -1,6 +1,26 @@
+import { useContext, useState } from "react";
+import { CurrentUserContext } from "../../../../../../contexts/CurrentUserContext";
+
 export default function EditAvatar() {
+  const { currentUser, handleUpdateAvatar } = useContext(CurrentUserContext);
+  const [avatar, setAvatar] = useState(currentUser.avatar);
+
+  const handleAvatarChange = (event) => {
+    setAvatar(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    handleUpdateAvatar({ avatar });
+  };
+
   return (
-    <form className="popup__input input-avatar" noValidate>
+    <form
+      className="popup__input input-avatar"
+      noValidate
+      onSubmit={handleSubmit}
+    >
       <div className="input__wrap">
         <input
           className="input__text input__text-avatar"
@@ -9,14 +29,12 @@ export default function EditAvatar() {
           type="url"
           placeholder="Link de imagem"
           required
+          value={avatar}
+          onChange={handleAvatarChange}
         />
         <p className="input__errorMessage"></p>
       </div>
-      <button
-        type="submit"
-        className="input__submit input__submit-avatar"
-        disabled
-      >
+      <button type="submit" className="input__submit input__submit-avatar">
         Salvar
       </button>
     </form>
